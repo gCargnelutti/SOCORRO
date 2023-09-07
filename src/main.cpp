500ce270ea672e53dd7d679519157c20116f79e3
brymoi <bryanmoises10@gmail.com>
2023-09-07 17:45:05 -0300

    adicionado um config.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,7 @@
 #include <Arduino.h>
 #include <QTRSensors.h>
+#include <config.h>
 QTRSensors qtr;
-
-// PINS PONTE H
-#define Ain1 22 
-#define Ain2 23
-#define Bin1  18
-#define Bin2 21
-
-#define PWMA  4
-#define PWMB  19
-
-
-// PINS SENSOR IR
-#define IR 13
-#define S1 14
-#define S2 27
-#define S3 26
-#define S4 25
-#define S5 33
-#define S6 32
-#define S7 35
-#define S8 34
-
-// PWM?
-#define DEFAULT_LEDC_FREQ 800
-#define _resolution 10
-#define A_channel 0
-#define B_channel 1
 
 // QTR?
 const uint8_t SensorCount = 8;
