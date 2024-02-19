2979cb331a5a2a16c7ea51828ce57a647391f0d4
anagryczak <anagevert@alunos.utfpr.edu.br>
2024-02-19 16:29:31 -0300

    port test

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,16 @@
 #include <Arduino.h>
 #include <port.h>
-#include <QTRSensors.h>
 
 void setup() {
   
-
+pinMode (S5, OUTPUT);
 
 }
 
 void loop() {
-
-
+digitalWrite(S5,HIGH);
+delay(1000);
+digitalWrite(S5, LOW);
+delay(1000);
 
 }
